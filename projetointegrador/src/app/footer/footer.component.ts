f8909e84b6ff559dd74c716cbd00893e4dfe5a3d
Mariotti7 <midiasmart99@gmail.com>
2020-11-03 08:32:52 -0300

    Layout Project

---
+++ projetointegrador/src/app/footer/footer.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { faFacebook, faInstagram} from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin} from '@fortawesome/free-brands-svg-icons'
+import { faGithub} from '@fortawesome/free-brands-svg-icons'
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+
+  faInstagram = faInstagram
+  faLinkedin = faLinkedin
+  faGithub = faGithub
+  faFacebook = faFacebook
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
